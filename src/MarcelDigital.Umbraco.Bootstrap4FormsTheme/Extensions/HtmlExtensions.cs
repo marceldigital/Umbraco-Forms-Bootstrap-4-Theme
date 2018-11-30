61eacefe8a6211f011a904edc28f91fdaf47d5a1
Alex Vilmur <alex@vilmurnet.com>
2018-11-30 15:20:22 -0600

    Added files.

---
+++ src/MarcelDigital.Umbraco.Bootstrap4FormsTheme/Extensions/HtmlExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarcelDigital.Umbraco.Bootstrap4FormsTheme.Extensions {
+    public class HtmlExtensions {
+        public static string ValidationClass(this HtmlHelper htmlHelper,
+                                                string fieldId,
+                                                string validClass = "",
+                                                string invalidClass = "is-invalid") {
+            var isValid = htmlHelper.ViewContext.ViewData.ModelState.IsValidField(fieldId);
+
+            return isValid ? validClass : invalidClass;
+        }
+    }
+}
