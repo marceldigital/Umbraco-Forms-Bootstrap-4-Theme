672d96beb3c02a5f15029a707777815730fe9896
Alex Vilmur <alex@vilmurnet.com>
2020-07-01 11:03:51 -0500

    Initial updates for Umbraco 8.

--- src/MarcelDigital.Umbraco.Bootstrap4FormsTheme/Extensions/HtmlExtensions.cs
+++ src/MarcelDigital.Umbraco.Bootstrap4FormsTheme/Extensions/HtmlExtensions.cs
@@ -2,6 +2,9 @@
 
 namespace MarcelDigital.Umbraco.Bootstrap4FormsTheme.Extensions {
     public static class HtmlExtensions {
+        /// <summary>
+        ///     Adds the correct Bootstrap 4 validation class to invalid form fields.
+        /// </summary>
         public static string ValidationClass(this HtmlHelper htmlHelper,
                                                 string fieldId,
                                                 string validClass = "",
